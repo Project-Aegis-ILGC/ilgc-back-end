a9f0fc58c6bc7a4370e36d05448c3d40e0d07ab1
jalansubham7 <subham.jalan@plaksha.edu.in>
2023-10-23 02:08:03 +0530

    prototype v1.4

--- prototype/main.py
+++ prototype/main.py
@@ -1,3 +1,13 @@
 import streamlit as st 
 
 st.header("Project Aegis")
+
+st.subheader("Add Guard")
+"Add guards data (name, number, MAC)"
+
+
+st.subheader("Assign Patrols")
+"Assign Patrols using some pre added patrol locations"
+
+st.subheader("Patrols status")
+"tells you the status of the assigned patrols"
